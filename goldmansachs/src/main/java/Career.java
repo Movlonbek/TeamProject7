3d68880337d8349c06eaf59e9dee6ba256ebe501
movlonbek <movlonbektojimatov@gmail.com>
2019-05-22 17:14:24 -0400

    added testcases

--- goldmansachs/src/main/java/Career.java
+++ goldmansachs/src/main/java/Career.java
@@ -1,7 +1,49 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class Career extends CommonAPI {
+
+
+        //-------------Initializing all the WebElements of Career page--------------
+
+        @FindBy(xpath = "//*[@id=\"wrapper\"]/section[1]/div[1]/div/div[1]/a/span")
+        public static WebElement proffesionalsClick;
+        @FindBy(xpath = "//*[@id=\"wrapper\"]/section[1]/div[1]/div/div[2]/a/span")
+        public static WebElement explore;
+        @FindBy(xpath = "//*[@id=\"wrapper\"]/section[1]/div[1]/div/div/div/div[3]/a/span")
+        public static WebElement prepare;
+        @FindBy(xpath = "//*[@id=\"wrapper\"]/section[1]/div[1]/div/div[4]/a/span")
+        public static WebElement searchJob;
+        @FindBy(xpath = "//*[@id=\"wrapper\"]/section[1]/div[1]/div/div[5]/a/span")
+        public static WebElement blog;
+
+
+
+        //-------------List of click methods of all the WebElements of Contact Us page--------------
+
+
+        public void proffesionalsClick(){
+           proffesionalsClick.click();
+        }
+
+        public void explore(){
+            explore.click();
+        }
+        public void prepare(){
+           prepare.click();
+        }
+        public void searchJob(){
+            searchJob.click();
+        }
+        public void blog(){
+            blog.click();
+        }
+
 
 @Test
     public void clickCareers() {
@@ -23,4 +65,29 @@ public class Career extends CommonAPI {
         driver.findElement(By.xpath("//*[@id=\"footer-student\"]/div/div/div[2]/div/div[2]/div/div/div/div/a")).click();
         sleepFor(2);
     }
+public Career careerPage;
+
+       @BeforeMethod
+        public void init(){
+            careerPage = PageFactory.initElements(driver, Career.class);
+        }
+
+        //-------------Test case 1. Search for Data, Analytics positions ---------------
+
+
+        public void searchJobs(){
+            careerPage.searchJobs();
+            sleepFor(1);
+            String pageSearch = driver.getPageSource();
+
+            if(pageSearch.indexOf("Data") != -1 && pageSearch.indexOf("Analytics") != -1){
+                System.out.println("Goldmansachs provides positions at Data and Analytics field ");
+            }
+            else{
+                System.out.println("They dont provide Data and Analytics field");
+            }
+            sleepFor(1);
+        }
+
+
 }
--- goldmansachs/src/main/java/HomePage.java
+++ goldmansachs/src/main/java/HomePage.java
@@ -1,11 +1,20 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebElement;
+
 
 public class HomePage extends CommonAPI {
 
+    public void Login() {
 
-    public void clickOurFirm() {
-        driver.findElement(By.xpath("//*[@id=\"marketing-nav\"]/li[2]/a")).click();
+        driver.findElement(By.xpath("//*[@id=\"loginNav\"]/a")).click();
+        sleepFor(1);
+        driver.findElement(By.xpath("//*[@id=\"loginNav\"]/ul/li[1]/ul/li[2]/a")).click();
+        sleepFor(1);
+        driver.findElement(By.xpath("//*[@id=\"gsid-masked\"]")).sendKeys("Username");
+        driver.findElement(By.xpath("//*[@id=\"gspw\"]")).sendKeys("Password");
+        driver.findElement(By.xpath("//*[@id=\"gsLoginForm\"]/ul/li[5]/button")).click();
         sleepFor(1);
     }
 
@@ -33,16 +42,11 @@ public class HomePage extends CommonAPI {
         driver.findElement(By.xpath("//*[@id=\"content\"]/section[1]/div/div[2]/div/div/a")).click();
     }
 
-
-    public void Login() {
-
-        driver.findElement(By.xpath("//*[@id=\"loginNav\"]/a")).click();
-        sleepFor(1);
-        driver.findElement(By.xpath("//*[@id=\"loginNav\"]/ul/li[1]/ul/li[2]/a")).click();
-        sleepFor(1);
-        driver.findElement(By.xpath("//*[@id=\"gsid-masked\"]")).sendKeys("Username");
-        driver.findElement(By.xpath("//*[@id=\"gspw\"]")).sendKeys("Password");
-        driver.findElement(By.xpath("//*[@id=\"gsLoginForm\"]/ul/li[5]/button")).click();
-        sleepFor(1);
+    public void testLogin() {
+        WebElement element = driver.findElement(By.linkText("Login"));
+        Actions action = new Actions(driver);
+        action.moveToElement(element).build().perform();
+        driver.findElement(By.linkText("Goldman.com")).click();
+        sleepFor(3);
     }
 }
